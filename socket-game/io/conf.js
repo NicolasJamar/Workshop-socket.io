f260b21f80e58bb02c182d28bc843e4924261428
Nico <nico@pop-os.localdomain>
2022-11-04 12:15:37 +0100

    done

--- socket-game/io/conf.js
+++ socket-game/io/conf.js
@@ -1,6 +1,36 @@
 const socketio = require('socket.io');
 
-module.exports = function(server) {
+module.exports = function (server) {
   // io server
   const io = socketio(server);
+
+  const players = {};
+
+  io.on("connection", (socket) => {
+    console.log("Players:", players)
+    console.log("Player connected: ", socket.id);
+    players[socket.id] = {
+      x: 0,
+      y: 0,
+      size: 20,
+      speed: 7,
+      c: "#" + (((1 << 24) * Math.random()) | 0).toString(16),
+    };
+
+    socket.on("disconnect", () => {
+      delete players[socket.id]
+    });
+
+    socket.on('move left',  () => { if(players[socket.id].x >= 5) players[socket.id].x -= players[socket.id].speed; });
+    socket.on('move up',  () => { if(players[socket.id].y >= 5) players[socket.id].y -= players[socket.id].speed; });
+    socket.on('move right',  () => { if(players[socket.id].x < 620) players[socket.id].x += players[socket.id].speed; });
+    socket.on('move down',  () => { if(players[socket.id].y < 458) players[socket.id].y += players[socket.id].speed; });
+  });
+
+  function update() {
+    io.volatile.emit("players list", Object.values(players))
+  }
+
+  setInterval(update, 1000 / 60)
+
 };
--- socket-game/public/game.js
+++ socket-game/public/game.js
@@ -1,1 +1,51 @@
+const socket = io();
 
+// const player = {
+//   x: 0,
+//   y: 0,
+//   size: 20,
+//   speed: 7
+// };
+
+let players = [];
+
+socket.on("players list", (list) => {
+  players = list;
+})
+
+const ctx = canvas.getContext("2d");
+
+function drawPlayer() {
+  players.forEach(({ x, y, size, c }) => {
+    ctx.beginPath();
+    ctx.rect(x, y, size, size);
+    ctx.fillStyle = c;
+    ctx.fill();
+  })
+}
+
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawPlayer();
+  movePlayer()
+  requestAnimationFrame(update)
+}
+
+requestAnimationFrame(update);
+
+const keyboard = {};
+
+window.onkeydown = (e) => {
+  keyboard[e.key] = true;
+}
+
+window.onkeyup = (e) => {
+  delete keyboard[e.key];
+}
+
+function movePlayer() {
+  if (keyboard['ArrowLeft']) socket.emit('move left');
+  if (keyboard['ArrowUp']) socket.emit('move up');
+  if (keyboard['ArrowRight']) socket.emit('move right');
+  if (keyboard['ArrowDown']) socket.emit('move down');
+};
